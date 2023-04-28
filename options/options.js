3cd818c143bd951f0a9fd8974b80b092b91fd525
Health Care Martin <healthcareservicesexperts@gmail.com>
2023-04-28 14:05:57 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "prescription-creato-40317",
   url: "https://prescription-creato-40317.botics.co",
   api: "https://prescription-creato-40317.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
